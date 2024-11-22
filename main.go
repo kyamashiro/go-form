9ddee3ad4d654e1c4721738c5c70bbfb115ab3e6
kyamashiro <kyamashiro73@gmail.com>
2024-11-23 07:40:18 +0900

    add air

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"html/template"
 	"net/http"
 )
 
@@ -12,9 +14,26 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
+	//r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	//	w.Write([]byte("hello world"))
+	//	http.ServeFile(w, r, "templates/login.html")
+	//})
 
-	http.ListenAndServe(":3000", r)
+	r.Get("/", Login)
+
+	err := http.ListenAndServe(":80", r)
+	catch(err)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/login.html")
+	err := t.Execute(w, nil)
+	catch(err)
+}
+
+func catch(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
 }
